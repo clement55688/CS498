8dfad72836ca7f3b780a55e1c08238d5f3d861c2
clement55688 <a2040404@gmail.com>
2024-01-19 08:54:49 +0000

    add code

---
+++ MP2/server.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+seed_value = 0  # Default seed value
+
+@app.route('/', methods=['GET'])
+def get_seed():
+    return str(seed_value)
+
+@app.route('/', methods=['POST'])
+def update_seed():
+    global seed_value
+    data = request.get_json()
+    seed_value = data.get('num', seed_value)  # Update seed value if 'num' is provided
+    return jsonify(success=True, new_value=seed_value)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Run the server on port 80
